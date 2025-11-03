14484537ef0fd852715a73a93fef92c870fb0935
schmiddim <schmiddim@gmx.at>
2025-11-04 00:00:00 +0100

    Add display configuration with Kanshi for multi-monitor support

---
+++ home/kanshi.nix
@@ -0,0 +1,54 @@
+{ ... }:
+
+{
+  services.kanshi = {
+    enable = true;
+
+    settings = [
+
+      {
+        profile = {
+          name = "zuhause";
+
+          outputs = [
+            {
+              criteria = "Iiyama North America PL2760H 1164011302154";
+              mode = "1920x1080";
+              position = "0,0";
+              scale = 1.0;
+              status = "enable";
+            }
+
+            {
+              criteria = "Iiyama North America PL2790 11306JK401162";
+              mode = "1920x1080@60Hz";
+              position = "1920,0";
+              scale = 1.0;
+              status = "enable";
+            }
+
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+          ];
+        };
+      }
+
+      {
+        profile = {
+          name = "unterwegs";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              mode = "1920x1080@60.027Hz";
+              position = "0,0";
+              scale = 1.0;
+              status = "enable";
+            }
+          ];
+        };
+      }
+    ];
+  };
+}
