343a1ca1046b652d2bbb54efee26ca796c38c076
schmiddim <schmiddim@gmx.at>
2025-12-29 11:28:51 +0100

    configuration.nix cleanup

--- configuration.nix
+++ configuration.nix
@@ -22,35 +22,35 @@ in
     "nixos-config=/etc/nixos/configuration.nix"
     "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
   ];
+
+  # cleanup old stuff
+  nix.gc = {
+    automatic = true;
+    dates = "weekly"; # oder "daily"
+    options = "--delete-older-than 14d";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.ms = import ./home/home.nix;
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    # Bootloader.
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    # Use latest kernel.
+    #  boot.kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages;
+    extraModulePackages = [ config.boot.kernelPackages.evdi ];
+    kernelModules = [ "evdi" ];
+    kernelParams = [
+      "intel_iommu=on"
+      "iommu=pt"
+    ];
+  };
 
-  # Use latest kernel.
-  #  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelPackages = pkgs.linuxPackages;
-  boot.extraModulePackages = [ config.boot.kernelPackages.evdi ];
-  boot.kernelModules = [ "evdi" ];
-  # Erforderlich, um den DisplayLink-Manager Dienst zu starten
-  # (Dies aktiviert keinen X-Server, solange services.xserver.enable = false bleibt)
-  services.xserver.videoDrivers = [
-    "displaylink"
-    "modesetting"
-  ];
-
-  services.udev.extraRules = ''
-    # DisplayLink (D6000) Power Management Fix
-    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="17e9", ATTR{power/control}="on"
-  '';
-
-  boot.kernelParams = [
-    "intel_iommu=on"
-    "iommu=pt"
-  ];
   systemd.services.thunderbolt-pre-sleep = {
     description = "Thunderbolt pre-suspend";
     before = [ "sleep.target" ];
@@ -60,10 +60,6 @@ in
       ExecStart = "${pkgs.coreutils}/bin/true";
     };
   };
-
-  #services.udev.extraRules = ''
-  #  ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="on"
-  #'';
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -89,40 +85,77 @@ in
     LC_TIME = "de_DE.UTF-8";
   };
 
-  services.greetd.enable = true;
-  programs.regreet.enable = true;
-  services.xserver.enable = false;
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.greetd.enableGnomeKeyring = true; # #hmm
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
+  programs = {
+    zsh.enable = true;
+    nix-ld.enable = true; # for intellij
+    regreet.enable = true;
+    sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+    };
   };
-
-  security.polkit.enable = true;
 
   xdg.portal = {
     enable = true;
     wlr.enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.avahi.enable = true;
-  services.avahi.nssmdns4 = true;
+  services = {
+    # Enable CUPS to print documents.
+    printing.enable = true;
+    avahi.enable = true;
+    avahi.nssmdns4 = true;
+    pulseaudio.enable = false;
+    # Enable sound with pipewire.
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+    };
+    k3s = {
+      enable = true;
+      role = "server";
+      extraFlags = [ "--write-kubeconfig-mode=644" ];
+    };
+    # List services that you want to enable:
+    # Enable the OpenSSH daemon.
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = true;
+        KbdInteractiveAuthentication = true;
+      };
+    };
+
+    greetd.enable = true;
+    xserver.enable = false;
+    gnome.gnome-keyring.enable = true;
+    # Firmware Updates
+    fwupd.enable = true;
+    hardware.bolt.enable = true;
+
+    # Erforderlich, um den DisplayLink-Manager Dienst zu starten
+    # (Dies aktiviert keinen X-Server, solange services.xserver.enable = false bleibt)
+    xserver.videoDrivers = [
+      "displaylink"
+      "modesetting"
+    ];
+    udev.extraRules = ''
+      # DisplayLink (D6000) Power Management Fix
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="17e9", ATTR{power/control}="on"
+    '';
+
+  };
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
+  security = {
+    rtkit.enable = true;
+    pam.services.greetd.enableGnomeKeyring = true; # #hmm
+    polkit.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -136,11 +169,6 @@ in
     ];
   };
 
-  programs.zsh.enable = true;
-  programs.nix-ld.enable = true; # for intellij
-
-  nixpkgs.config.allowUnfree = true;
-
   environment.systemPackages = with pkgs; [
     git
     usbutils
@@ -150,39 +178,21 @@ in
     regreet
     libinput
   ];
-  # Enable the gnome-keyring secrets vault.
-  # Will be exposed through DBus to programs willing to store secrets.
-
-  services.k3s = {
-    enable = true;
-    role = "server";
-    extraFlags = [ "--write-kubeconfig-mode=644" ];
-  };
-  # List services that you want to enable:
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.settings = {
-    PasswordAuthentication = true;
-    KbdInteractiveAuthentication = true;
-  };
-
-  nix.gc = {
-    automatic = true;
-    dates = "weekly"; # oder "daily"
-    options = "--delete-older-than 14d";
-  };
 
   nix.optimise = {
     automatic = true;
     dates = [ "weekly" ];
   };
 
-  # Firmware Updates
-  services.fwupd.enable = true;
-  services.hardware.bolt.enable = true;
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
   };
 
   fonts = {
@@ -206,7 +216,5 @@ in
     WLR_RENDERER = "vulkan";
     XDG_CURRENT_DESKTOP = "sway";
   };
-
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
