359a3c41ca2ac56d3fb7416b663e3a3a1622bd2b
schmiddim <schmiddim@gmx.at>
2026-01-02 10:50:51 +0100

    flake stuff

--- flake.nix
+++ flake.nix
@@ -1,27 +1,31 @@
 {
-  description = "NixOS configuration";
+  description = "NixOS configuration for ThinkPad P52";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }: {
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in {
     nixosConfigurations.p52 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
+
       modules = [
         nixos-hardware.nixosModules.lenovo-thinkpad-p52
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
-          nixpkgs.hostPlatform = "x86_64-linux";
+          nixpkgs.hostPlatform = system;
+          home-manager.useGlobalPkgs = false;
+          home-manager.useUserPackages = true;
         }
       ];
     };
